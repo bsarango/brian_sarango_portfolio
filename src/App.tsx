b5fa384c62be81f5f8396f9426fffb7ceabf512f
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-10-07 23:13:14 -0400

    removed useState and unused imported logos in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,7 +5,7 @@ function App() {
   return (
     <>
       <div>
-
+        Brian Sarango Main Page
       </div>
     </>
   )
