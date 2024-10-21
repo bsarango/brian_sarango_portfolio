72bf01de492a91d08a08dc1c8993c7483f02fbc0
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-10-21 13:26:57 -0400

    p with text and br's added inside div tag in About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,9 +3,12 @@ function About(){
     return(
     <div>
         <p>
-            I'm a Software Engineer with FullStack development experience. \n
-            I previously have experience in the healthcare field working with a range of other professionals \n
-
+            I'm a Software Engineer with FullStack development experience. <br></br>
+            I previously have experience in the healthcare field working with a range of other professionals.<br></br>
+            I'm experienced in created front ends with React and JavaScript and Backends with Flask with Python. 
+            I have experience working with databases as well using SQLite, SQLAlchemy, and PostreSQL.<br></br>
+            I'm also an a musician who loves learning abot Jazz and am active working in both music and with young people in my local church.<br></br>
+            
         </p>
     </div>
     )
