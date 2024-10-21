ded603815cd665f6c15dfa61d1bff8f783e49ea4
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-10-21 13:30:41 -0400

    li tags with links added inside the ul in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,9 @@ function Navbar(){
             <div className="logo">Brian Sarango</div>
             <div>
                 <ul className="nav-links">
-                    
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#projects">Projects</a></li>
                 </ul>
             </div>
         </nav>
