10955fe33b223e0946e23db726fad3e99bf6b3b1
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-10-24 14:01:00 -0400

    nav tag added with divs for logo, hamburgermenu for mobile view, hamburger menu icon and links in menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,16 +2,33 @@
 function Navbar(){
 
     return(
-        <nav id="desktop-nav">
-            <div className="logo">Brian Sarango</div>
-            <div>
-                <ul className="nav-links">
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                </ul>
-            </div>
-        </nav>
+        <>
+            <nav id="desktop-nav">
+                <div className="logo">Brian Sarango</div>
+                <div>
+                    <ul className="nav-links">
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                        <li><a href="#projects">Projects</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <nav id="hamburger-nav">
+                <div className="logo">Brian Sarango</div>
+                <div className="hamburger-menu">
+                    <div className="hamburger-icon" onClick={togglemenu()}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <div className="menu-links">
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                        <li><a href="#projects">Projects</a></li>
+                    </div>
+                </div>
+            </nav>
+        </>
     )
 }
 
