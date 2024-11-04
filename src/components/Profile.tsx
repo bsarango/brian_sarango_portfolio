0c1f3145fb23af1b961857cc904003bf04a8890e
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-11-04 12:09:03 -0500

    div tag created with inner btn tags; div tag created with inner img tags in Profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -8,6 +8,16 @@ function Profile(){
                 <p className="section__text__p1">Hello, I'm</p>
                 <h1 className="title">Brian Sarago</h1>
                 <p className="section__text__p2">Software Engineer</p>
+                <div className="btn-container">
+                    <button className="btn btn-color-2">Download Resume</button> {/*add onClick to button to download resume */}
+                    <button className="btn btn-color-1">    {/*add onClick for button to move to Contact section*/}
+                        Download Resume
+                    </button> 
+                </div>
+                <div className="socials-container">
+                    <img src="" alt="My Linkedin Profile" className=""/>    {/* add onClick to change location to send to linkedin profile + add Linkedin icon pic */}
+                    <img src="" alt="My Github Profile" className=""/>    {/* add onClick to change location to send to github profile + add Github icon pic*/}
+                </div>
             </div>
         </section>
     )
