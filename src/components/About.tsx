6a94d6f3e9cf3955158c9620fde9df06a9cf38a7
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-11-04 13:21:58 -0500

    section tag, p tag, and h1 tag added to About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,16 +1,24 @@
 function About(){
 
     return(
-    <div className="about__text-container">
-        <p className = "about-p">
-            I'm a Software Engineer with FullStack development experience. <br></br>
-            I previously have experience in the healthcare field working with a range of other professionals.<br></br>
-            I'm experienced in created front ends with React and JavaScript and Backends with Flask with Python. 
-            I have experience working with databases as well using SQLite, SQLAlchemy, and PostreSQL.<br></br>
-            I'm also an a musician who loves learning abot Jazz and am active working in both music and with young people in my local church.<br></br>
+        <section id = "about">
+            <p className="section__text__p1">
+                Get to know more
+            </p>
+            <h1 className="title">
+                About me
+            </h1>
+        </section>
+    // <div className="about__text-container">
+    //     <p className = "about-p">
+    //         I'm a Software Engineer with FullStack development experience. <br></br>
+    //         I previously have experience in the healthcare field working with a range of other professionals.<br></br>
+    //         I'm experienced in created front ends with React and JavaScript and Backends with Flask with Python. 
+    //         I have experience working with databases as well using SQLite, SQLAlchemy, and PostreSQL.<br></br>
+    //         I'm also an a musician who loves learning abot Jazz and am active working in both music and with young people in my local church.<br></br>
 
-        </p>
-    </div>
+    //     </p>
+    // </div>
     )
 }
 
